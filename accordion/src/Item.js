f29f19c39db929bd66e7a9573ae3dc39adac44ab
Vova <podolian@gmail.com>
2024-02-09 22:35:10 +0200

    new

--- accordion/src/Item.js
+++ accordion/src/Item.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 export function Item({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toogleIsOpenHandler = () => {
+    setIsOpen((isOpen) => !isOpen);
+  };
+
   return (
-    <div className={"item "}>
-      <p className="number">{num}</p>
+    <div
+      className={isOpen ? "item open" : "item"}
+      onClick={toogleIsOpenHandler}
+    >
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
-      <p className="icon">-</p>
-      <div className="content-text">{text}</div>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-text">{text}</div>}
     </div>
   );
 }
